4bfd41f829439c8bc18c5d650da087dc3c4f4b72
yungd1plomat <aynur.musin.06@mail.ru>
2023-06-19 21:39:07 +0500

    add default user initializing

---
+++ ContestGenerator/Helpers/DataInitializer.cs
@@ -0,0 +1,23 @@
+﻿using ContestGenerator.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace ContestGenerator.Helpers
+{
+    public static class DataInitializer
+    {
+        public static async Task SeedData(ApplicationDbContext db, UserManager<IdentityUser> userManager)
+        {
+            await SeedUsers(userManager);
+        }
+
+        public static async Task SeedUsers(UserManager<IdentityUser> userManager)
+        {
+            var requiredUser = await userManager.FindByNameAsync("admin");
+            if (requiredUser is null)
+            {
+                var user = new IdentityUser("admin");
+                await userManager.CreateAsync(user, "Admin_1234");
+            }
+        }
+    }
+}
--- ContestGenerator/Program.cs
+++ ContestGenerator/Program.cs
@@ -1,6 +1,8 @@
 using ContestGenerator.Data;
+using ContestGenerator.Helpers;
 using ContestGenerator.Impls;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System.Text;
 
 namespace ContestGenerator
@@ -9,7 +11,26 @@ namespace ContestGenerator
     {
         public static async Task Main(string[] args)
         {
-            await CreateHostBuilder(args).Build().RunAsync();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var db = services.GetRequiredService<ApplicationDbContext>();
+                    //await db.Database.MigrateAsync();
+
+                    var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+                    await DataInitializer.SeedData(db, userManager);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
