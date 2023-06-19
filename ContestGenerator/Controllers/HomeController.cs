a72fea40331e0644eaae75dc01f44f4b9160f3ee
yungd1plomat <aynur.musin.06@mail.ru>
2023-06-19 23:56:56 +0500

    add home page

---
+++ ContestGenerator/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContestGenerator.Controllers
+{
+    [Route("[controller]")]
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
