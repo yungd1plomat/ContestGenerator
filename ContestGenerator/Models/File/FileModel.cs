87141a9cad6a4cf133e1f30e52d7429b98797f59
yungd1plomat <aynur.musin.06@mail.ru>
2023-10-09 23:38:54 +0500

    add file uploading

---
+++ ContestGenerator/Models/File/FileModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContestGenerator.Models.File
+{
+    /// <summary>
+    /// Класс предоставляет информацию
+    /// о загруженном файле
+    /// </summary>
+    public class FileModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название файла
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Путь файла
+        /// </summary>
+        public string Path { get; set; }
+    }
+}
---
+++ ContestGenerator/Models/UploadFileViewmodel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContestGenerator.Models
+{
+    public class UploadFileViewmodel
+    {
+        [Required]
+        [Display(Name = "Файл")]
+        public IFormFile File { get; set; }
+
+        [Required]
+        [Display(Name = "Название файла")]
+        public string Name { get; set; }
+    }
+}
