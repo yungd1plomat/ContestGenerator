d098c09c303d2d7e6819e567d7335f4fe2641403
yungd1plomat <aynur.musin.06@mail.ru>
2023-10-10 13:32:05 +0500

    add edit contest feature

---
+++ ContestGenerator/Models/EditViewmodel.cs
@@ -0,0 +1,12 @@
+﻿using ContestGenerator.Models.File;
+
+namespace ContestGenerator.Models
+{
+    public class EditViewmodel
+    {
+        public Contest.Contest Contest { get; set; }
+
+
+        public IEnumerable<FileModel> Files { get; set; }
+    }
+}
