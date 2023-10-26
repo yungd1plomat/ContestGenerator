cb703d184e6673dc7e45f9869c49cafd8b42e054
yungd1plomat <aynur.musin.06@mail.ru>
2023-10-26 22:22:29 +0500

    add news groupping

--- ContestGenerator/wwwroot/js/contest.js
+++ ContestGenerator/wwwroot/js/contest.js
@@ -15,4 +15,35 @@
             });
         }
     });
+
+    const carousel = $('.carousel');
+    const numItems = $('.carousel-item').length;
+    let visibleItems = 3; // Default number of visible items
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth < 640) { // Adjust the breakpoint as needed
+        visibleItems = 1; // On mobile devices, display one item at a time
+    }
+
+    const itemWidth = visibleItems === 1 ? carousel.outerWidth() : carousel.outerWidth() / visibleItems;
+    const carouselWidth = itemWidth * numItems;
+    let currentPosition = 0;
+
+    $('.carousel-container').width(itemWidth * visibleItems);
+
+    $('.carousel').width(carouselWidth);
+
+    $('.next-button').click(function () {
+        if (currentPosition > -(carouselWidth - (itemWidth * visibleItems))) {
+            currentPosition -= itemWidth * visibleItems;
+            carousel.css('transform', 'translateX(' + currentPosition + 'px)');
+        }
+    });
+
+    $('.prev-button').click(function () {
+        if (currentPosition < 0) {
+            currentPosition += itemWidth * visibleItems;
+            carousel.css('transform', 'translateX(' + currentPosition + 'px)');
+        }
+    });
 });
